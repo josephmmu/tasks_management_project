a5e13b93f98910300ed161f14cc2abc9a12038fe
josephmmu <lr.jsales@mmdc.mcl.edu.ph>
2025-01-24 13:39:27 +0800

    reverted

--- task_management_project/tasks/models.py
+++ task_management_project/tasks/models.py
@@ -16,8 +16,9 @@ class Task(models.Model):
 
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    assigned_to = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Task: {self.title} assigned to {self.assigned_to.username}"
+        return self.title
